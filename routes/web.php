6210fa48947bf38b5ed75bde7bcae1cfb45e9f17
junaidbinjaman <me@junaidbinjaman.com>
2025-05-07 10:10:00 +0600

    feat: developoed a function to publish a blog. The feature is now working as expected

--- routes/web.php
+++ routes/web.php
@@ -2,9 +2,15 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\UserController;
+use App\Http\Controllers\PostController;
 
 Route::get('/', [UserController::class, 'showCorrectHomepage']);
 
+// User related routes
 Route::post('/register', [UserController::class, 'register']);
 Route::post('/login', [UserController::class, 'login']);
 Route::post('/logout', [UserController::class, 'logout']);
+
+// Blog post related routes
+Route::get('/create-post', [PostController::class, 'showCreateForm']);
+Route::post('/create-post', [PostController::class, 'storeNewPost']);
