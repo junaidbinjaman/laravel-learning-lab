f15adcfaaee5edae28d22b1ed01cb33d90e46f4c
junaidbinjaman <me@junaidbinjaman.com>
2025-05-12 10:10:00 +0600

    feat: the chat channel is now fully developed using livewire and it is fully functional as well

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -5,16 +5,13 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
     <meta http-equiv="X-UA-Compatible" content="ie=edge"/>
     <title> @isset($doctitle)
-        {{$doctitle}} | Our App
+            {{$doctitle}} | Our App
         @else
-        Our App
+            Our App
         @endisset
     </title>
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css"
           integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous"/>
-    <script defer src="https://use.fontawesome.com/releases/v5.5.0/js/all.js"
-            integrity="sha384-GqVMZRt5Gn7tB9D9q7ONtcp4gtHIUEW/yG7h98J7IpE3kpi+srfFyyB/04OV6pG0"
-            crossorigin="anonymous"></script>
     <link rel="preconnect" href="https://fonts.googleapis.com"/>
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin/>
     <link href="https://fonts.googleapis.com/css2?family=Source+Sans+Pro:ital,wght@0,400;0,700;1,400;1,700&display=swap"
@@ -25,16 +22,26 @@
 <body>
 <header class="header-bar mb-3">
     <div class="container d-flex flex-column flex-md-row align-items-center p-3">
-        <h4 class="my-0 mr-md-auto font-weight-normal"><a href="/" class="text-white">OurApp</a></h4>
+        <h4 class="my-0 mr-md-auto font-weight-normal"><a wire:navigate href="/" class="text-white">OurApp</a></h4>
 
         @auth
             <div class="flex-row my-3 my-md-0">
-                <livewire:search />
-                <livewire:chat />
-                <a href="/profile/{{auth()->user()->username}}" class="mr-2"><img title="My Profile" data-toggle="tooltip" data-placement="bottom"
-                                              style="width: 32px; height: 32px; border-radius: 16px"
-                                              src="{{auth()->user()->avatar}}"/></a>
-                <a class="btn btn-sm btn-success mr-2" href="/create-post">Create Post</a>
+
+                @persist('headerdynamic')
+                <div class="flex-row my-3 my-md-0">
+                    <livewire:search/>
+                    <livewire:chat/>
+                </div>
+                @endpersist
+
+                <a wire:navigate href="/profile/{{auth()->user()->username}}" class="mr-2">
+                    <img title="My Profile"
+                         data-toggle="tooltip"
+                         data-placement="bottom"
+                         style="width: 32px; height: 32px; border-radius: 16px"
+                         src="{{auth()->user()->avatar}}"/>
+                </a>
+                <a wire:navigate class="btn btn-sm btn-success mr-2" href="/create-post">Create Post</a>
                 <form action="/logout" method="POST" class="d-inline">
                     @csrf
                     <button class="btn btn-sm btn-secondary">Sign Out</button>
@@ -63,12 +70,13 @@
 </header>
 <!-- header ends here -->
 
-@if(session()->has('success')))
-<div class="container container--narrow">
-    <div class="alert alert-success text-center">
-        {{ session('success')  }}
+@if(session()->has('success'))
+    )
+    <div class="container container--narrow">
+        <div class="alert alert-success text-center">
+            {{ session('success')  }}
+        </div>
     </div>
-</div>
 @endif
 
 @if(session()->has('failure'))
--- resources/views/livewire/search.blade.php
+++ resources/views/livewire/search.blade.php
@@ -36,7 +36,7 @@
                                 ({{count($results)}} {{count($results) > 1 ? "results" : "result"}} found)
                             </div>
                             @foreach($results as $post)
-                                <a href="/post/{{$post->id}}" class="list-group-item list-group-item-action">
+                                <a x-on:click.prevent="isOpen = false; Livewire.navigate('/post/{{$post->id}}')" href="/post/{{$post->id}}" class="list-group-item list-group-item-action">
                                     <img class="avatar-tiny" src="{{$post->user->avatar}}">
                                     <strong>{{$post->title}}</strong>
                                     <span
