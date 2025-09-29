4d5ea833d25f86551d07cd448640a100e74a2ede
junaidbinjaman <me@junaidbinjaman.com>
2025-09-29 10:10:00 +0600

    feat: Student crud operation api development is completed.

--- app/Http/Controllers/API/StudentApiController.php
+++ app/Http/Controllers/API/StudentApiController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\API;
 
 use App\Http\Controllers\Controller;
 use App\Http\Requests\Api\Students\StoreStudentRequest;
+use App\Http\Requests\Api\Students\UpdateStudentRequest;
 use App\Models\Student;
 use Illuminate\Http\Request;
 
@@ -45,14 +46,50 @@ class StudentApiController extends Controller
     public function show(string $id)
     {
         //
+        $student = Student::query()
+            ->find($id);
+
+        if ($student) {
+            return response()->json([
+                'status' => 'success',
+                'data' => $student
+            ]);
+        }
+
+        return response()->json([
+            'status' => 'failed',
+            'message' => 'No student found.'
+        ], 404);
     }
 
     /**
      * Update the specified resource in storage.
      */
-    public function update(Request $request, string $id)
+    public function update(StoreStudentRequest $request, string $id)
     {
         //
+        $data = $request->validated();
+
+        $student = Student::query()
+            ->find($id);
+
+        if (!$student) {
+            return response()->json([
+                'status' => 'fail',
+                'message' => 'No student found'
+            ], 404);
+        }
+
+        $student->name = $request->name;
+        $student->email = $request->email;
+        $student->gender = $request->gender;
+        $student->save();
+
+        return response()->json([
+            'status' => 'success',
+            'message' => 'student data updated successfully',
+            'data' => $student
+        ], 200);
     }
 
     /**
@@ -61,5 +98,21 @@ class StudentApiController extends Controller
     public function destroy(string $id)
     {
         //
+        $student = Student::query()
+            ->find($id);
+
+        if (!$student) {
+            return response()->json([
+                'status' => 'fail',
+                'message' => 'Student not found'
+            ], 404);
+        }
+
+        $student->delete();
+
+        return response()->json([
+            'status' => 'success',
+            'message' => 'Student deleted successfully'
+        ]);
     }
 }
